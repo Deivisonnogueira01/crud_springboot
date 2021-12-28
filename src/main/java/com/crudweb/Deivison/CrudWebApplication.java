3b2c04103350b78bcb6cb38777342b42cd413ac4
Deivison Nogueira <deivison.nogueira@live.com>
2021-12-28 09:46:28 -0400

    inicialização

---
+++ src/main/java/com/crudweb/Deivison/CrudWebApplication.java
@@ -0,0 +1,13 @@
+package com.crudweb.Deivison;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crudweb/Deivison/CrudWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudweb.Deivison;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
