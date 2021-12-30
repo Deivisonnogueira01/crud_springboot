b909f239932415c398b7440a0de906ad96f302db
Deivison Nogueira <deivison.nogueira@live.com>
2021-12-30 10:28:33 -0400

    OrderStatus Enum

--- src/main/java/com/crudweb/Deivison/config/TestConfig.java
+++ src/main/java/com/crudweb/Deivison/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.crudweb.Deivison.entities.Order;
 import com.crudweb.Deivison.entities.User;
+import com.crudweb.Deivison.entities.enuns.OrderStatus;
 import com.crudweb.Deivison.repositories.OrderRepository;
 import com.crudweb.Deivison.repositories.UserRepository;
 
@@ -30,9 +31,9 @@ public class TestConfig implements CommandLineRunner {
 		User u2 = new User(null,"Mario", "ms.12@live.com", "157480", "147000");
 		
 		                                           /// ISO 8601
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1); 
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2); 
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1); 
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.PAID, u1); 
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.SHIPPED, u2); 
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.WAITING_PAYMENT, u1); 
 
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
