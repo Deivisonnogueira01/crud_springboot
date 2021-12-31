20a2d95717773c0c477e31f6e78e539b5e5ca85d
Deivison Nogueira <deivison.nogueira@live.com>
2021-12-31 09:54:38 -0400

    Tratando Exceções Parte 2

--- src/main/java/com/crudweb/Deivison/services/UserService.java
+++ src/main/java/com/crudweb/Deivison/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.crudweb.Deivison.entities.User;
 import com.crudweb.Deivison.repositories.UserRepository;
+import com.crudweb.Deivison.services.exceptions.DatabaseException;
 import com.crudweb.Deivison.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -31,7 +34,13 @@ public class UserService {
 	 }
 	   
 	 public void delete(Long id) {
-		 repository.deleteById(id);
+		 try {
+		      repository.deleteById(id);
+		 }catch (EmptyResultDataAccessException e) {
+			 throw new ResourceNotFoundException(id);
+		 }catch (DataIntegrityViolationException e) {
+			 throw new DatabaseException(e.getMessage());
+		 }
 	 }
 	
 	   /// Atualizar
